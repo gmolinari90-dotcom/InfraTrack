44a6faa15f818c853e9883ad80a3c05bfba5912f
gmolinari90-dotcom <g.molinari90@gmail.com>
2025-10-17 15:55:59 +0200

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# Non tentiamo di attivare l'ambiente.
-# Usiamo 'conda run' per eseguire il comando passato
-# direttamente DENTRO l'ambiente specificato.
-exec conda run -n infratrack "$@"
